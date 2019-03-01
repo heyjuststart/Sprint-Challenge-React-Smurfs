a644229c29ee341f1eb3c278cb49a9c57e0330ff
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-01 12:18:41 -0500

    add edit-smurf functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -6,6 +6,12 @@ const Smurf = props => {
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <button onClick={() => props.onEditClick({
+        name: props.name,
+        id: props.id,
+        age: props.age,
+        height: props.height
+      })}>edit</button>
       <button onClick={() => props.deleteSmurf(props.id)}>x</button>
     </div>
   );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = props.editingSmurf || {
       name: '',
       age: '',
       height: ''
@@ -20,35 +20,44 @@ class SmurfForm extends Component {
       age: '',
       height: ''
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  renderButton() {
+    if (this.props.isEditing) {
+      return <button type="submit">Submit Edit</button>;
+    } else {
+      return <button type="submit">Add to the village</button>;
+    }
+  }
+
   render() {
+    const smurf = this.state;
     return (
       <div className="SmurfForm">
         <form onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
             placeholder="name"
-            value={this.state.name}
+            value={smurf.name}
             name="name"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
-            value={this.state.age}
+            value={smurf.age}
             name="age"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
-            value={this.state.height}
+            value={smurf.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          { this.renderButton() }
         </form>
       </div>
     );
